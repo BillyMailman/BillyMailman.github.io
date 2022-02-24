25d7570bd5da8e36de6caec09d1d22568c687ce1
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-24 08:29:19 -0400

    Yay, more functions!

--- COH/Builder/js/archetype.js
+++ COH/Builder/js/archetype.js
@@ -1,5 +1,9 @@
 function loadArchetypes(callback){
-  fetch('https://cod.uberguy.net/homecoming/archetypes/index.json', {method:'get'}).then(callback);
+  fetch('https://cod.uberguy.net/homecoming/archetypes/index.json', {method:'get'}).then(response => parseArchetypes(response, callback));
+}
+
+function parseArchetypes(response, callback){
+  callback(response.json())
 }
 
 export {loadArchetypes};
