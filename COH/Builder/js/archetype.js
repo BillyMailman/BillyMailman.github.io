36b1a7ca5a1b0070b831d289c92123b240ef644a
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-24 09:10:24 -0400

    Maybe?

--- COH/Builder/js/archetype.js
+++ COH/Builder/js/archetype.js
@@ -1,11 +1,19 @@
 function loadArchetypes(callback){
   fetch('https://cod.uberguy.net/homecoming/archetypes/index.json', {method:'get'})
   .then(response => response.json())
-  .then(json => parseArchetypesList(json, callback));
+  .then(parseArchetypesList)
+  .then(callback);
 }
 
 function parseArchetypesList(json, callback){
-  callback(json);
+  var result = {};
+  json.player_archetypes.forEach((item, i) => {
+    result[item] = 'player';
+  });
+  json.npc_archetypes.forEach((item, i) => {
+    result[item] = 'npc';
+  });
+  return result;
 }
 
 export {loadArchetypes};
