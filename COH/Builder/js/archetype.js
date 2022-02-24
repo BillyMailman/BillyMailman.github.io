a80544fbb185b3beef4c35a6503d880c0895430c
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-24 08:58:23 -0400

    Restructuring Test

--- COH/Builder/js/archetype.js
+++ COH/Builder/js/archetype.js
@@ -1,9 +1,11 @@
 function loadArchetypes(callback){
-  fetch('https://cod.uberguy.net/homecoming/archetypes/index.json', {method:'get'}).then(response => parseArchetypes(response, callback));
+  fetch('https://cod.uberguy.net/homecoming/archetypes/index.json', {method:'get'})
+  .then(response => response.json())
+  .then(json => parseArchetypesList(json, callback));
 }
 
-function parseArchetypes(response, callback){
-  response.json().then(callback);
+function parseArchetypesList(json, callback){
+  callback(json);
 }
 
 export {loadArchetypes};
