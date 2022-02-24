9d4a6c7cbdcff4c0872a38857431f284ab1e944b
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-24 09:28:12 -0400

    Some map testing.

--- COH/Builder/js/archetype.js
+++ COH/Builder/js/archetype.js
@@ -2,18 +2,27 @@ function loadArchetypes(callback){
   fetch('https://cod.uberguy.net/homecoming/archetypes/index.json', {method:'get'})
   .then(response => response.json())
   .then(parseArchetypesList)
+  .then(fetchArchetypeData)
   .then(callback);
 }
 
-function parseArchetypesList(json, callback){
-  var result = {};
+function parseArchetypesList(json){
+  var result = new Map();
   json.player_archetypes.forEach((item, i) => {
-    result[item] = 'player';
+    result.set(item, 'player');
   });
   json.npc_archetypes.forEach((item, i) => {
-    result[item] = 'npc';
+    result.set(item, 'npc');
   });
   return result;
+}
+
+function fetchArchetypeData(archetypeMap){
+  archetypeMap.forEach((v, k) => {
+    console.log(v);
+    console.log(k);
+  });
+  return 'done';
 }
 
 export {loadArchetypes};
