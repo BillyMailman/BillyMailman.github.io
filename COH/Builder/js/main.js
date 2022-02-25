d9f13e93db4c2eb3543550f6bfecd3a7ba6ad055
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-25 09:25:27 -0400

    Trying a thing!

--- COH/Builder/js/main.js
+++ COH/Builder/js/main.js
@@ -1,5 +1,7 @@
-import { createCharacter } from './character.js';
+import { Character } from './character.js';
 import { loadArchetypes } from './archetype.js';
 import {fillArchetypeDropdown} from './ui.js';
+
+var character = new Character();
 
 loadArchetypes(data => fillArchetypeDropdown(data, console.log));
