f0692f2fc7f7d11da6b1d4d61b275f9b120a12f8
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-24 21:12:44 -0400

    This probably works. Ugly, but functional.

--- COH/Builder/js/ui.js
+++ COH/Builder/js/ui.js
@@ -1,15 +1,18 @@
 function fillArchetypeDropdown(parsedArchetypeData, selectionCallback){
   var dropdown = document.getElementById('archetype-dropdown');
+  var archetypesMap = new Map();
   parsedArchetypeData.forEach((archetype) => {
+    archetypesMap.set(archetype.name, archetype);
     if(!archetype.player) return;
     var newOption = document.createElement('option');
     newOption.value = archetype.name;
     newOption.text = archetype.display_name;
     dropdown.appendChild(newOption);
   });
+  window.archetypes = archetypesMap;
   dropdown.addEventListener("change", function(event){
     var selection = event.target.value;
-    var selectedArchetype = selection;
+    var selectedArchetype = window.archetypes.get(selection);
     selectionCallback(selectedArchetype)
   });
 }
