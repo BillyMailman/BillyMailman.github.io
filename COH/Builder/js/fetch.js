e1d14b4960336beaa75f6babaabfc90f30cf5114
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-26 10:03:17 -0400

    Forgot to save! BLARGH!

--- COH/Builder/js/fetch.js
+++ COH/Builder/js/fetch.js
@@ -2,3 +2,5 @@ function getData(folder, file){
   return fetch('https://cod.uberguy.net/homecoming/${folder}/${file}.json', {method:'get'})
   .then(response => response.json())
 }
+
+export {getData};
