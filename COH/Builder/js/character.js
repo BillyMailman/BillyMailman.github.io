ac6f48cb28845e2d14772db19b650af1b4e9cbd6
Billy Mailman <billy.mailman+GITHUB@gmail.com>
2022-02-27 16:04:28 -0400

    JS is Hard!

--- COH/Builder/js/character.js
+++ COH/Builder/js/character.js
@@ -24,26 +24,26 @@ class Character {
       return _archetype.attributes[attribute_name][value];
     }
     if(value === "cur"){
-      var totalWithBuffs = getAttribute(attribute_name, 'base') + getTotalBuffs(attribute_name, 'cur');
-      var max = getAttribute(attribute_name, 'max');
-      var min = getAttribute(attribute_name, 'min');
+      var totalWithBuffs = getAttribute(attribute_name, 'base') + this.getTotalBuffs(attribute_name, 'cur');
+      var max = this.getAttribute(attribute_name, 'max');
+      var min = this.getAttribute(attribute_name, 'min');
       return Math.max(Math.min(totalWithBuffs, max), min);
     }
     if(value === "max"){
-      var maxWithBuffs = _archetype.attributes[attribute_name]['max'] + getTotalBuffs(attribute_name, 'max');
-      var maxmax = getAttribute(attribute_name, 'maxmax');
+      var maxWithBuffs = _archetype.attributes[attribute_name]['max'] + this.getTotalBuffs(attribute_name, 'max');
+      var maxmax = this.getAttribute(attribute_name, 'maxmax');
       return Math.min(maxWithBuffs, maxmax);
     }
     if(value === "res"){
-      var buffValue = getTotalBuffs(attribute_name, 'res');
-      var resmax = getAttribute(attribute_name, 'resmax');
-      var resmin = getAttribute(attribute_name, 'resmin');
+      var buffValue = this.getTotalBuffs(attribute_name, 'res');
+      var resmax = this.getAttribute(attribute_name, 'resmax');
+      var resmin = this.getAttribute(attribute_name, 'resmin');
       return Math.max(Math.min(buffValue, resmax), resmin);
     }
     if(value === "str"){
-      var buffValue = getTotalBuffs(attribute_name, 'str');
-      var strmax = getAttribute(attribute_name, 'strmax');
-      var strmin = getAttribute(attribute_name, 'strmin');
+      var buffValue = this.getTotalBuffs(attribute_name, 'str');
+      var strmax = this.getAttribute(attribute_name, 'strmax');
+      var strmin = this.getAttribute(attribute_name, 'strmin');
       return Math.max(Math.min(buffValue, strmax), strmin);
     }
   }
